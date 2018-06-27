e19fb026c76d5eda58b7888a9e94aa83fb669ed2
kenan <xinkenan.11@hotmail.com>
2018-06-27 22:13:11 +0800

    generate supplier module

---
+++ dist/skeleton/default/src/app/suppliers/suppliers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SuppliersRoutingModule } from './suppliers-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SuppliersRoutingModule
+  ],
+  declarations: []
+})
+export class SuppliersModule { }
