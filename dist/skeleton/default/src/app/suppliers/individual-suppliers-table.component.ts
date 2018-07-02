156f3398d7cfd64f6a83047d176a73e86349d5e3
kenan <xinkenan.11@hotmail.com>
2018-07-02 09:38:52 +0800

    stash for work

--- dist/skeleton/default/src/app/suppliers/individual-suppliers-table.component.ts
+++ dist/skeleton/default/src/app/suppliers/individual-suppliers-table.component.ts
@@ -1,8 +1,4 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit
-} from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ScriptLoaderService } from '../_services/script-loader.service';
 import { SupplierService } from './supplier.service';
 
@@ -13,7 +9,6 @@ import { SupplierService } from './supplier.service';
 })
 export class IndividualSuppliersTableComponent
   implements OnInit, AfterViewInit {
-
   suppliers: any[];
   errorMessage: string;
 
@@ -22,124 +17,249 @@ export class IndividualSuppliersTableComponent
     private _supplierService: SupplierService
   ) {}
 
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
+  ngOnInit(): void {
     this._supplierService.getSuppliers().subscribe(suppliers => {
-      this.createDatatable(suppliers);
-    }, error => this.errorMessage = <any>error);
+      // this.createDatatable(suppliers);
+      this.suppliers = suppliers;
+    }, error => (this.errorMessage = <any>error));
   }
 
-  private createDatatable( parentData: any[] = [], childData: any[] = [] ) {
-    const dataJSONArray = parentData;
-    // == Class definition
-    const Datatable = function () {
+  ngAfterViewInit(): void {
+    this.initDatatable();
+  }
 
-      // == Private functions
-      var subTableInit = function (e) {
-        $('<div/>').attr('id', 'child_data_' + e.data.RecordID).appendTo(e.detailCell).mDatatable({
+  private initDatatable() {
+    //== Class definition
+
+    var DatatableHtmlTableDemo = (function() {
+      //== Private functions
+
+      // demo initializer
+      var demo = function() {
+        var datatable = $('.m-datatable').mDatatable({
           data: {
-            type: 'local',
-            source: e.data.Orders,
-            pageSize: 10,
-            saveState: {
-              cookie: true,
-              webstroage: true
-            },
+            saveState: { webstroage: true },
+            pageSize: 15
           },
-          // layout definition
+         "ColumnDefs": [{
+            visible: false,
+            targets: [-1]
+          }]
           layout: {
             theme: 'default',
-            scroll: true,
-            height: 300,
-            footer: false,
-            // enable/disable datatable spinner.
-            spinner: {
-              type: 1,
-              theme: 'default',
-            },
+            scroll: false,
+            height: null,
+            footer: false
           },
           sortable: true,
-          // columns definition
+          pagination: true,
+          detail: {
+            title: 'Load sub tablte',
+            content: e => {
+              console.log('init subtatble script');
+            }
+          },
+          search: {
+            input: $('#generalSearch')
+          },
           columns: [
             {
-              field: 'OrderID',
-              title: 'Order ID',
+              field: 'CurrencyID',
+              title: '',
               sortable: false,
-            }, {
-              field: 'ShipCountry',
-              title: 'Country',
-              width: 100,
-            }, {
-              field: 'ShipAddress',
-              title: 'Ship Address',
-            }, {
-              field: 'ShipName',
-              title: 'Ship Name',
-            }, {
-              field: 'OrderDate',
-              title: 'Order Date',
-            }, {
-              field: 'TotalPayment',
-              title: 'Total Payment',
-            }, {
-              field: 'Status',
-              title: 'Status',
-              // callback function support for column rendering
-              template: function (row) {
-                var status = {
-                  1: { 'title': 'Pending', 'class': 'm-badge--brand' },
-                  2: { 'title': 'Delivered', 'class': ' m-badge--metal' },
-                  3: { 'title': 'Canceled', 'class': ' m-badge--primary' },
-                  4: { 'title': 'Success', 'class': ' m-badge--success' },
-                  5: { 'title': 'Info', 'class': ' m-badge--info' },
-                  6: { 'title': 'Danger', 'class': ' m-badge--danger' },
-                  7: { 'title': 'Warning', 'class': ' m-badge--warning' },
-                };
-                return '<span class="m-badge ' + status[row.Status].class + ' m-badge--wide">' + status[row.Status].title + '</span>';
-              },
-            }, {
-              field: 'Type',
-              title: 'Type',
-              // callback function support for column rendering
-              template: function (row) {
-                var status = {
-                  1: { 'title': 'Online', 'state': 'danger' },
-                  2: { 'title': 'Retail', 'state': 'primary' },
-                  3: { 'title': 'Direct', 'state': 'accent' },
-                };
-                return '<span class="m-badge m-badge--' + status[row.Type].state + ' m-badge--dot"></span>&nbsp;<span class="m--font-bold m--font-' + status[row.Type].state + '">' +
-                  status[row.Type].title + '</span>';
-              },
+              filterable: false,
+              width: 20,
+              textAlign: 'center',
             },
-          ],
+            {
+              field: 'Currency Name',
+              title: 'Currency Name',
+              width: 100
+            },
+            {
+              field: 'IndividualAgent',
+              title: 'Individual Agent',
+              width: 40
+            },
+            {
+              field: 'AverageRate',
+              title: 'Average Rate'
+            }
+          ]
         });
+
+        $('#m_form_status').on('change', function() {
+          datatable.search(
+            $(this)
+              .val()
+              .toLowerCase(),
+            'Status'
+          );
+        });
+
+        $('#m_form_type').on('change', function() {
+          datatable.search(
+            $(this)
+              .val()
+              .toLowerCase(),
+            'Type'
+          );
+        });
+
+        $('#m_form_status, #m_form_type').selectpicker();
+      };
+
+      return {
+        //== Public functions
+        init: function() {
+          // init dmeo
+          demo();
+        }
+      };
+    })();
+
+    jQuery(document).ready(function() {
+      DatatableHtmlTableDemo.init();
+    });
+    //# sourceMappingURL=html-table.js.map
+  }
+
+  private createDatatable(parentData: any[] = [], childData: any[] = []) {
+    const dataJSONArray = parentData;
+    // == Class definition
+    const Datatable = (function() {
+      // == Private functions
+      var subTableInit = function(e) {
+        $('<div/>')
+          .attr('id', 'child_data_for_record_' + e.data.RecordID)
+          .appendTo(e.detailCell)
+          .mDatatable({
+            data: {
+              type: 'local',
+              source: e.data.Orders,
+              pageSize: 15,
+              saveState: {
+                cookie: true,
+                webstroage: true
+              }
+            },
+            // layout definition
+            layout: {
+              theme: 'default',
+              scroll: false,
+              height: null,
+              footer: false,
+              // enable/disable datatable spinner.
+              spinner: {
+                type: 1,
+                theme: 'default'
+              }
+            },
+            sortable: true,
+            // columns definition
+            columns: [
+              {
+                field: 'OrderID',
+                title: 'Order ID',
+                sortable: false
+              },
+              {
+                field: 'ShipCountry',
+                title: 'Country',
+                width: 100
+              },
+              {
+                field: 'ShipAddress',
+                title: 'Ship Address'
+              },
+              {
+                field: 'ShipName',
+                title: 'Ship Name',
+                template: `{{OrderID}} - {{ShipCountry}}`
+              },
+              {
+                field: 'OrderDate',
+                title: 'Order Date'
+              },
+              {
+                field: 'TotalPayment',
+                title: 'Total Payment'
+              },
+              {
+                field: 'Status',
+                title: 'Status',
+                // callback function support for column rendering
+                template: function(row) {
+                  var status = {
+                    1: { title: 'Pending', class: 'm-badge--brand' },
+                    2: { title: 'Delivered', class: ' m-badge--metal' },
+                    3: {
+                      title: 'Canceled',
+                      class: ' m-badge--primary'
+                    },
+                    4: { title: 'Success', class: ' m-badge--success' },
+                    5: { title: 'Info', class: ' m-badge--info' },
+                    6: { title: 'Danger', class: ' m-badge--danger' },
+                    7: { title: 'Warning', class: ' m-badge--warning' }
+                  };
+                  return (
+                    '<span class="m-badge ' +
+                    status[row.Status].class +
+                    ' m-badge--wide">' +
+                    status[row.Status].title +
+                    '</span>'
+                  );
+                }
+              },
+              {
+                field: 'Type',
+                title: 'Type',
+                // callback function support for column rendering
+                template: function(row) {
+                  var status = {
+                    1: { title: 'Online', state: 'danger' },
+                    2: { title: 'Retail', state: 'primary' },
+                    3: { title: 'Direct', state: 'accent' }
+                  };
+                  return (
+                    '<span class="m-badge m-badge--' +
+                    status[row.Type].state +
+                    ' m-badge--dot"></span>&nbsp;<span class="m--font-bold m--font-' +
+                    status[row.Type].state +
+                    '">' +
+                    status[row.Type].title +
+                    '</span>'
+                  );
+                }
+              }
+            ]
+          });
       };
       // demo initializer
-      var mainTableInit = function () {
+      var mainTableInit = function() {
         var datatable = $('.m_datatable').mDatatable({
           // datasource definition
           data: {
             type: 'local',
             source: dataJSONArray,
-            pageSize: 10,
+            pageSize: 10
           },
           // layout definition
           layout: {
             theme: 'default',
             scroll: true,
             height: null,
-            footer: false,
+            footer: false
           },
           sortable: true,
           filterable: true,
           pagination: true,
           detail: {
             title: 'Load sub table',
-            content: subTableInit,
+            content: subTableInit
           },
           search: {
-            input: $('#generalSearch'),
+            input: $('#generalSearch')
           },
           // columns definition
           columns: [
@@ -149,89 +269,33 @@ export class IndividualSuppliersTableComponent
               sortable: false,
               width: 20,
               textAlign: 'center' // left|right|center,
-            }, {
-              field: 'FirstName',
-              title: 'First Name',
-            }, {
-              field: 'LastName',
-              title: 'Last Name',
-            }, {
-              field: 'Company',
-              title: 'Company',
-            }, {
-              field: 'Email',
-              title: 'Email',
-            }, {
-              field: 'Phone',
-              title: 'Phone',
-            }, {
-              field: 'Status',
-              title: 'Status',
-              // callback function support for column rendering
-              template: function (row) {
-                var status = {
-                  1: { 'title': 'Pending', 'class': 'm-badge--brand' },
-                  2: { 'title': 'Delivered', 'class': ' m-badge--metal' },
-                  3: { 'title': 'Canceled', 'class': ' m-badge--primary' },
-                  4: { 'title': 'Success', 'class': ' m-badge--success' },
-                  5: { 'title': 'Info', 'class': ' m-badge--info' },
-                  6: { 'title': 'Danger', 'class': ' m-badge--danger' },
-                  7: { 'title': 'Warning', 'class': ' m-badge--warning' },
-                };
-                return '<span class="m-badge ' + status[row.Status].class + ' m-badge--wide">' + status[row.Status].title + '</span>';
-              },
-            }, {
-              field: 'Type',
-              title: 'Type',
-              // callback function support for column rendering
-              template: function (row) {
-                var status = {
-                  1: { 'title': 'Online', 'state': 'danger' },
-                  2: { 'title': 'Retail', 'state': 'primary' },
-                  3: { 'title': 'Direct', 'state': 'accent' },
-                };
-                return '<span class="m-badge m-badge--' + status[row.Type].state + ' m-badge--dot"></span>&nbsp;<span class="m--font-bold m--font-' + status[row.Type].state + '">' +
-                  status[row.Type].title + '</span>';
-              },
-            }, {
-              field: 'Actions',
-              width: 110,
-              title: 'Actions',
-              sortable: false,
-              overflow: 'visible',
-              template: function (row, index, datatable) {
-                var dropup = (datatable.getPageSize() - index) <= 4 ? 'dropup' : '';
-                return '\
-						<div class="dropdown ' + dropup + '">\
-							<a href="#" class="btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" data-toggle="dropdown">\
-                                <i class="la la-ellipsis-h"></i>\
-                            </a>\
-						  	<div class="dropdown-menu dropdown-menu-right">\
-						    	<a class="dropdown-item" href="#"><i class="la la-edit"></i> Edit Details</a>\
-						    	<a class="dropdown-item" href="#"><i class="la la-leaf"></i> Update Status</a>\
-						    	<a class="dropdown-item" href="#"><i class="la la-print"></i> Generate Report</a>\
-						  	</div>\
-						</div>\
-						<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="Edit details">\
-							<i class="la la-edit"></i>\
-						</a>\
-						<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="Delete">\
-							<i class="la la-trash"></i>\
-						</a>\
-					';
-              },
+            },
+            {
+              field: 'Currency',
+              title: 'Currency',
+              width: 100
+            },
+            {
+              field: 'Individual Agent',
+              title: 'Individual Agent',
+              width: 150
+            },
+            {
+              field: 'Average Rate',
+              title: 'Average Rate',
+              width: 150
             }
-          ],
+          ]
         });
       };
       return {
         //== Public functions
-        init: function () {
+        init: function() {
           // init dmeo
           mainTableInit();
-        },
+        }
       };
-    } ();
+    })();
     Datatable.init();
   }
 }
