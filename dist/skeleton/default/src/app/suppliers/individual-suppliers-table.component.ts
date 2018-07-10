84c75001e1841d34fa7879839774aa7f30066c90
kenan <xinkenan.11@hotmail.com>
2018-07-10 14:22:04 +0800

    refactor out datatables into individual functions

--- dist/skeleton/default/src/app/suppliers/individual-suppliers-table.component.ts
+++ dist/skeleton/default/src/app/suppliers/individual-suppliers-table.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { SuppliersService } from './suppliers.service';
-import { CurrenciesService } from './currencies.service';
-import { RatesService } from './rates.service';
-import { flatMap, map } from 'rxjs/operators';
-import { forkJoin } from 'rxjs/observable/forkJoin';
+import {Component, OnInit} from '@angular/core';
+import {SuppliersService} from './suppliers.service';
+import {CurrenciesService} from './currencies.service';
+import {RatesService} from './rates.service';
+import {flatMap, map} from 'rxjs/operators';
+import {forkJoin} from 'rxjs/observable/forkJoin';
 
 @Component({
   selector: 'app-individual-suppliers-table',
   templateUrl: './individual-suppliers-table.component.html',
   styles: []
 })
-export class IndividualSuppliersTableComponent  implements OnInit {
+export class IndividualSuppliersTableComponent implements OnInit {
 
   suppliers: any[];
   currencies: any[];
   errorMessage: string;
   readonly INDIVIDUAL_AGENT = 0;
-  childTable: any;
-  parentTable: any;
+  childTableInitializer: any;
+  parentTableInitializer: any;
+  _this = this;
 
   constructor(private _supplierService: SuppliersService,
               private _currenciesService: CurrenciesService,
@@ -49,12 +50,13 @@ export class IndividualSuppliersTableComponent  implements OnInit {
             currency.agents = [];
             currency.numAgents = 0;
 
-            let ratesRequests = this._ratesService.getRatesByCurrencyId(currency.id).pipe(
+            const ratesRequests = this._ratesService.getRatesByCurrencyId(currency.id).pipe(
               flatMap((rate: any) => {
+
                 currency.agents.push(rate);
                 currency.numAgents++;
 
-                let supplierRequest = this._supplierService.getSuppliersById(rate.supplierId, this.INDIVIDUAL_AGENT).pipe(
+                const supplierRequest = this._supplierService.getSuppliersById(rate.supplierId, this.INDIVIDUAL_AGENT).pipe(
                   map((supplier: any) => {
                     rate.supplier = supplier;
                     return currency
@@ -75,124 +77,125 @@ export class IndividualSuppliersTableComponent  implements OnInit {
     });
   }
 
-  private initIndivAgentTable(currencies: any[] = []) {
-    let parentTableInitializer;
-    let childTableInitializer = parentTableData => {
+  private initIndivAgentTable(currencies: any[] = [] ){
+    let childTable;
+    let parentTable;
 
-      const currencyObj = currencies.find((obj) => +obj.id === +parentTableData.data.currencyId);
+    $(function () {
+      let childInitializer = parentTableData => {
 
-      $('<div/>')
-        .attr('id', 'suppliers_for_currency_' + parentTableData.data.currencyId)
-        .appendTo(parentTableData.detailCell)
-        .mDatatable({
-          data: {
-            type: 'local',
-            source: currencyObj.agents,
-            pageSize: 15,
-            saveState: {
-              cookie: false,
-              localStorage: true
-            }
-          },
-          sortable: true,
-          columns: [
-            {
-              title: '',
-              field: 'supplier.id',
-              sortable: false,
-              filterable: false,
-              textAlign: 'center',
-              width: 20,
-              overflow: 'hidden',
-              template: row => {
-                return "<div style='color:transparent'>" + row.supplier.id + "</div>";
+        const currencyObj = currencies.find((obj) => +obj.id === +parentTableData.data.currencyId);
+
+        childTable = $('<div/>')
+          .attr('id', 'suppliers_for_currency_' + parentTableData.data.currencyId)
+          .appendTo(parentTableData.detailCell)
+          .mDatatable({
+            data: {
+              type: 'local',
+              source: currencyObj.agents,
+              pageSize: 15,
+              saveState: {
+                cookie: false,
+                localStorage: true
               }
             },
-            {
-              title: 'Agent Code',
-              field: 'supplier.code',
-              sortable: true,
-              filterable: true,
-              width: 100
-            },
-            {
-              title: 'Average Rate',
-              field: 'rate',
-              width: 80,
-              type: 'number',
-              sortable: 'asc'
-            },
-            {
-              title: 'Agent Name',
-              width: 120,
-              field: 'agentName',
-              template: row => {
-                return `${row.supplier.fName} ${row.supplier.lName}`;
-              }
-            },
-            {
-              title: "Payout Partner?",
-              field: "supplier.isPayoutPartner",
-              sortable: true,
-              filterable: true,
-              width: 100,
-              template: function (row) {
-                return row.supplier.isPayoutPartner === 'y' ? 'Yes' : "No"
+            sortable: true,
+            columns: [
+              {
+                title: '',
+                field: 'supplier.id',
+                sortable: false,
+                filterable: false,
+                textAlign: 'center',
+                width: 20,
+                overflow: 'hidden',
+                template: row => {
+                  return "<div style='color:transparent'>" + row.supplier.id + "</div>";
+                }
               },
-              sortCallback: (data, sort, column) => {
-                let field = column['field'];
-                let values = {"Yes": 1, "No": 0, "y": 1, "n": 0}
-                return $(data).sort((a, b) => {
-                  let aField = a.supplier.isPayoutPartner;
-                  aField = values[aField];
-                  let bField = b.supplier.isPayoutPartner;
-                  bField = values[bField];
+              {
+                title: 'Agent Code',
+                field: 'supplier.code',
+                sortable: true,
+                filterable: true,
+                width: 100
+              },
+              {
+                title: 'Average Rate',
+                field: 'rate',
+                width: 80,
+                type: 'number',
+                sortable: 'asc'
+              },
+              {
+                title: 'Agent Name',
+                width: 120,
+                field: 'agentName',
+                template: row => {
+                  return `${row.supplier.fName} ${row.supplier.lName}`;
+                }
+              },
+              {
+                title: "Payout Partner?",
+                field: "supplier.isPayoutPartner",
+                sortable: true,
+                filterable: true,
+                width: 100,
+                template: function (row) {
+                  return row.supplier.isPayoutPartner === 'y' ? 'Yes' : "No"
+                },
+                sortCallback: (data, sort, column) => {
+                  let field = column['field'];
+                  let values = {"Yes": 1, "No": 0, "y": 1, "n": 0}
+                  return $(data).sort((a, b) => {
+                    let aField = a.supplier.isPayoutPartner;
+                    aField = values[aField];
+                    let bField = b.supplier.isPayoutPartner;
+                    bField = values[bField];
 
-                  if (sort === 'asc') {
-                    return aField > bField ? 1 : aField < bField ? -1 : 0;
-                  } else {
-                    return aField < bField ? 1 : aField > bField ? -1 : 0;
-                  }
-                })
+                    if (sort === 'asc') {
+                      return aField > bField ? 1 : aField < bField ? -1 : 0;
+                    } else {
+                      return aField < bField ? 1 : aField > bField ? -1 : 0;
+                    }
+                  })
+                }
+              },
+              {
+                title: "Phone",
+                field: 'supplier.phone',
+                width: 150,
+                sortable: false,
+                filterable: true,
+              },
+              {
+                title: "Email",
+                field: 'supplier.email',
+                width: 210,
+                sortable: false,
+                filterable: true
+              },
+              {
+                title: "Status",
+                field: 'supplier.activeStatus',
+                width: 150,
+                sortable: 'desc',
+                filterable: true,
+                template: row => {
+                  var status = {
+                    2: {'title': 'Active', 'class': 'm-badge--success'},
+                    1: {'title:': 'Inactive', 'class': 'm-badge--metal'},
+                    0: {'title': "Blocked", 'class': 'm-badge--danger'}
+                  };
+                  return '<span class="m-badge ' + status[row.supplier.activeStatus].class + ' m-badge--wide">' + status[row.supplier.activeStatus].title + '</span>';
+                }
               }
-            },
-            {
-              title: "Phone",
-              field: 'supplier.phone',
-              width: 150,
-              sortable: false,
-              filterable: true,
-            },
-            {
-              title: "Email",
-              field: 'supplier.email',
-              width: 210,
-              sortable: false,
-              filterable: true
-            },
-            {
-              title: "Status",
-              field: 'supplier.activeStatus',
-              width: 150,
-              sortable: 'desc',
-              filterable: true,
-              template: row => {
-                var status = {
-                  2: {'title': 'Active', 'class': 'm-badge--success'},
-                  1: {'title:': 'Inactive', 'class': 'm-badge--metal'},
-                  0: {'title': "Blocked", 'class': 'm-badge--danger'}
-                };
-                return '<span class="m-badge ' + status[row.supplier.activeStatus].class + ' m-badge--wide">' + status[row.supplier.activeStatus].title + '</span>';
-              }
-            }
 
-          ]
+            ]
 
-        });
-    };
-
-    const initializeDatatable = function () {
-      parentTableInitializer = $('.m-datatable').mDatatable({
+          });
+      };
+      parentTable  = $('.m-datatable').mDatatable({
         data: {
           saveState: {webstroage: true, cookie: true},
           pageSize: 15
@@ -207,7 +210,7 @@ export class IndividualSuppliersTableComponent  implements OnInit {
         pagination: true,
         detail: {
           title: 'Load sub tablte',
-          content: childTableInitializer
+          content: childInitializer
         },
         search: {
           input: $('#generalSearch')
@@ -244,19 +247,21 @@ export class IndividualSuppliersTableComponent  implements OnInit {
       });
 
       $('#m_form_status').on('change', function () {
-        parentTableInitializer.search(($(this).val() as string).toLowerCase(), 'Status');
+        parentTable.search(($(this).val() as string).toLowerCase(), 'Status');
       });
 
       $('#m_form_type').on('change', function () {
-        parentTableInitializer.search(($(this).val() as string).toLowerCase(), 'Type');
+        parentTable.search(($(this).val() as string).toLowerCase(), 'Type');
       });
 
       $('#m_form_status, #m_form_type').selectpicker();
-    };
 
-    $(document).ready(function () {
-      initializeDatatable();
-    });
-    // # sourceMappingURL=html-table.js.map
+    })
+
+    return {
+      getChildTable: childTable,
+      getParentTable: parentTable,
+    }
   }
 }
+
